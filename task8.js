717070a1fb21f5c3752308332c5100cd21384ba8
cricket <rakhaevpavel@gmail.com>
2021-04-01 21:40:20 +0300

    task8.js added condition w/ 0

--- task8.js
+++ task8.js
@@ -1,18 +1,24 @@
 // function power(val, pow) {
-//     if (pow != 1) {
-//         return val * power(val, pow - 1);
+//     if (pow == 0) {
+//         return 1;
 //     }
-//     return val;
+//     else if (pow == 1) {
+//         return val;
+//     }
+//     return val * power(val, pow - 1);
 // }
-
-// alert(power(2, 4));
 
 
 // function power(val, pow) {
-//     if (pow == -1) {
+//     if (pow == 0) {
+//         return val - val + 1;
+//     }
+//     else if (pow == -1) {
 //         return 1 / val;
 //     }
 //     return (1 / val) * power(val, pow + 1);
 // }
 
+
 // alert(power(2, -4))
+// alert(power(2, 0));
