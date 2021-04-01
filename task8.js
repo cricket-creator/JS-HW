3419bd994ead54d578205abd79a61181f974de62
cricket <rakhaevpavel@gmail.com>
2021-04-01 21:31:21 +0300

    modified task8.js

--- task8.js
+++ task8.js
@@ -1,8 +1,18 @@
 // function power(val, pow) {
-//     if (pow == 1) {
-//         return val;
+//     if (pow != 1) {
+//         return val * power(val, pow - 1);
 //     }
-//     return val * power(val, pow - 1);
+//     return val;
 // }
 
 // alert(power(2, 4));
+
+
+// function power(val, pow) {
+//     if (pow == -1) {
+//         return 1 / val;
+//     }
+//     return (1 / val) * power(val, pow + 1);
+// }
+
+// alert(power(2, -4))
