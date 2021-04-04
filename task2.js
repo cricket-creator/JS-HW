5b802b1f8a295d647c3597dc2e9622ae63d6da8e
cricket <rakhaevpavel@gmail.com>
2021-04-04 20:48:23 +0300

    task(2,3).js complete

---
+++ task2.js
@@ -0,0 +1,26 @@
+// let menSocks, womenGlasses, femaleDress, menShirt;
+// menSocks = 1000;
+// womenGlasses = 1500;
+// femaleDress = 3200;
+// menShirt = 1900;
+
+// let cart = [];
+
+// cart.push(menSocks, femaleDress); // добавил 2 элемента в массив
+// console.log(cart);
+// cart.unshift(femaleDress, menShirt, womenGlasses); // добавил 3 элемента в начало массива
+// console.log(cart);
+// cart.pop(1); // удалил последний элемент с конца
+// console.log(cart);
+// cart.shift(2); // удалил 2 элемента из начала
+// console.log(cart);
+
+// function countBasketPrice() {
+//     let Sum = 0;
+//     for (let i = 0; i < cart.length; i++) {
+//         Sum += cart[i];
+//     }
+//     return console.log(`Общая сумма товара в корзине: ${Sum} руб.`);
+// }
+
+// countBasketPrice();
