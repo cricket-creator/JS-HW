ae38ded185fe50bccd07186e024d84db6d8c2547
cricket <rakhaevpavel@gmail.com>
2021-04-11 13:18:57 +0300

    HW modified

--- task1.js
+++ task1.js
@@ -1,35 +1,35 @@
-// let userNumber = prompt(`Введите число от 0 до 999: `, 245);
-// let argDivision = [];
-// let argChecker = {};
+/* let userNumber = prompt(`Введите число от 0 до 999: `, 245);
+let argDivision = [];
+let argChecker = {};
 
-// function getDivision(arg) {
-//     let division = arg.toString().split(``);
-//     argDivision = division.map(Number);
+function getDivision(arg) {
+    let division = arg.toString().split(``);
+    argDivision = division.map(Number);
 
-//     for (let i = 0; i < argDivision.length; i++) {
-//         if (isNaN(argDivision[i])) {
-//             alert(`Введите число от 0 до 999!`);
-//             return console.log(argChecker);
-//         };
-//     };
+    for (let i = 0; i < argDivision.length; i++) {
+        if (isNaN(argDivision[i])) {
+            alert(`Введите число от 0 до 999!`);
+            return console.log(argChecker);
+        };
+    };
 
-//     switch (argDivision.length) {
-//         case 1:
-//             argChecker.units = argDivision[0];
-//             return console.log(argChecker);
-//         case 2:
-//             argChecker.tens = argDivision[0];
-//             argChecker.units = argDivision[1];
-//             return console.log(argChecker);
-//         case 3:
-//             argChecker.hundreds = argDivision[0];
-//             argChecker.tens = argDivision[1];
-//             argChecker.units = argDivision[2];
-//             return console.log(argChecker);
-//         default:
-//             alert(`Введите число от 0 до 999!`);
-//             return console.log(argChecker);
-//     };
-// };
+    switch (argDivision.length) {
+        case 1:
+            argChecker.units = argDivision[0];
+            return console.log(argChecker);
+        case 2:
+            argChecker.tens = argDivision[0];
+            argChecker.units = argDivision[1];
+            return console.log(argChecker);
+        case 3:
+            argChecker.hundreds = argDivision[0];
+            argChecker.tens = argDivision[1];
+            argChecker.units = argDivision[2];
+            return console.log(argChecker);
+        default:
+            alert(`Введите число от 0 до 999!`);
+            return console.log(argChecker);
+    };
+};
 
-// getDivision(userNumber);
+getDivision(userNumber); */
--- task2.js
+++ task2.js
@@ -1,64 +1,52 @@
-// const menSocks = {
-//     sex: `men`,
-//     type: `clothes`,
-//     name: `socks`,
-//     price: 1000,
-//     amount: 3,
-//     discount: 1,
-//     totalPrice: function () {
-//         return this.amount * this.price * this.discount;
-//     },
-// };
+/* const menSocks = {
+    sex: `men`,
+    type: `clothes`,
+    name: `socks`,
+    price: 1000,
+    amount: 3,
+    discount: false,
+};
 
-// const womenGlasses = {
-//     sex: `women`,
-//     type: `accessories`,
-//     name: `glasses`,
-//     price: 1500,
-//     amount: 1,
-//     discount: 1,
-//     totalPrice: function () {
-//         return this.amount * this.price * this.discount;
-//     },
-// };
+const womenGlasses = {
+    sex: `women`,
+    type: `accessories`,
+    name: `glasses`,
+    price: 1500,
+    amount: 1,
+    discount: false,
+};
 
-// const femaleDress = {
-//     sex: `women`,
-//     type: `clothes`,
-//     name: `dress`,
-//     price: 3200,
-//     amount: 1,
-//     discount: 0.8,
-//     totalPrice: function () {
-//         return this.amount * this.price * this.discount;
-//     },
-// };
+const womenDress = {
+    sex: `women`,
+    type: `clothes`,
+    name: `dress`,
+    price: 3200,
+    amount: 1,
+    discount: 20,
+};
 
-// const menShirt = {
-//     sex: `men`,
-//     type: `clothes`,
-//     name: `shirt`,
-//     price: 1900,
-//     amount: 2,
-//     discount: 0.7,
-//     totalPrice: function () {
-//         return this.amount * this.price * this.discount;
-//     },
-// };
+const menShirt = {
+    sex: `men`,
+    type: `clothes`,
+    name: `shirt`,
+    price: 1900,
+    amount: 2,
+    discount: 30,
+};
 
-// let cart = [];
+let cart = [];
 
-// cart.push(menSocks);
-// cart.push(womenGlasses);
-// cart.push(femaleDress);
-// cart.push(menShirt);
+cart.push(menSocks);
+cart.push(womenGlasses);
+cart.push(womenDress);
+cart.push(menShirt);
 
-// function getCartTotalPrice(array) {
-//     let sum = 0;
-//     array.forEach(Object => {
-//         sum += Object.totalPrice();
-//     });
-//     return console.log(sum);
-// }
+function countBasketPrice(products) {
+    let sum = 0;
+    products.forEach(products => {
+        sum += products.amount * (products.price - (products.price * products.discount / 100));
+    });
+    return console.log(sum);
+}
 
-// getCartTotalPrice(cart);
+countBasketPrice(cart); */
